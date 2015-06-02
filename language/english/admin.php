b091b9e2bcb59f001f9571a888af155291dd1be2
mambax7 <mambax7@gmail.com>
2015-06-02 07:33:28 -0400

    English Update

--- language/english/admin.php
+++ language/english/admin.php
@@ -6,17 +6,16 @@
 // ------------------------------------------------------------------------- //
 
 include_once "../../tadtools/language/{$xoopsConfig['language']}/admin_common.php";
-define("_TAD_NEED_TADTOOLS","Need modules/tadtools. You can download tadtools from <a href='http://www.tad0616.net/modules/tad_uploader/index.php?of_cat_sn=50' target='_blank'>Tad's web</a>.");
+define('_TAD_NEED_TADTOOLS', "This module needs TadTools module. You can download TadTools from <a href='http://www.tad0616.net/modules/tad_uploader/index.php?of_cat_sn=50' target='_blank'>Tad's web</a>.");
 
 //tad_sitemap-edit
-define("_MA_TADSITEMA_MID","Module ID");
-define("_MA_TADSITEMA_NAME","Name");
-define("_MA_TADSITEMA_URL","Url");
-define("_MA_TADSITEMA_DESCRIPTION","Description");
-define("_MA_TADSITEMA_LAST_UPDATE","Last Update");
-define("_MA_TADSITEMA_SORT","Weight");
-define("_MA_TADSITEMA_INPUT_DESC","Please input the description");
-define("_MA_TADSITEMA_AUTO_IMPORT", "Auto Detect Site Map");
-define("_MA_TADSITEMA_HOMEPAGE", "Homepage");
-define("_MA_TADSITEMA_CLEAN", "Empty the item will not be displayed");
-?>
+define('_MA_TADSITEMA_MID', 'Module ID');
+define('_MA_TADSITEMA_NAME', 'Name');
+define('_MA_TADSITEMA_URL', 'URL');
+define('_MA_TADSITEMA_DESCRIPTION', 'Description');
+define('_MA_TADSITEMA_LAST_UPDATE', 'Last Update');
+define('_MA_TADSITEMA_SORT', 'Weight');
+define('_MA_TADSITEMA_INPUT_DESC', 'Please input the description');
+define('_MA_TADSITEMA_AUTO_IMPORT', 'Auto Detect Site Map');
+define('_MA_TADSITEMA_HOMEPAGE', 'Homepage');
+define('_MA_TADSITEMA_CLEAN', 'Empty items will not be displayed');
