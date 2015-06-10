e40b8d12fb274ecc6226b667fb12a1e43e22a14e
Tad <tad0616@gmail.com>
2015-06-10 14:34:18 +0800

    調整語系

--- language/english/modinfo.php
+++ language/english/modinfo.php
@@ -4,18 +4,18 @@
 // 製作日期：2015-05-04
 // $Id:$
 // ------------------------------------------------------------------------- //
-include_once XOOPS_ROOT_PATH."/modules/tadtools/language/{$xoopsConfig['language']}/modinfo_common.php";
+include_once XOOPS_ROOT_PATH . '/modules/tadtools/language/' . $xoopsConfig['language'] . '/modinfo_common.php';
 
-define('_MI_TADSITEMA_NAME','Site Map');
-define('_MI_TADSITEMA_AUTHOR','Tad (tad0616@gmail.com)');
-define('_MI_TADSITEMA_CREDITS','');
-define('_MI_TADSITEMA_DESC','Site Map');
-define('_MI_TADSITEMA_AUTHOR_WEB','Tad Textbook Network');
+define('_MI_TADSITEMA_NAME', 'Site Map');
+define('_MI_TADSITEMA_AUTHOR', 'Tad (tad0616@gmail.com)');
+define('_MI_TADSITEMA_CREDITS', '');
+define('_MI_TADSITEMA_DESC', 'Site Map');
+define('_MI_TADSITEMA_AUTHOR_WEB', 'Tad Textbook Network');
 define('_MI_TADSITEMA_ADMENU1', 'Admin');
 define('_MI_TADSITEMA_ADMENU1_DESC', 'Admin');
-define('_MI_TADSITEMA_ABOUT_SITE' , 'Site Map');
-define('_MI_TADSITEMA_ABOUT_SITE_DESC' , 'Site Map Description');
-define('_MI_TADSITEMA_ABOUT_SITE_DEFAULT' , '<p>Alt+L : Left Zone<br>Alt+C : Content and Center Zone.<br>Alt+R : Right Zone<br>Alt+U : Nav Bar<br></p>');
+define('_MI_TADSITEMA_ABOUT_SITE', 'Site Map');
+define('_MI_TADSITEMA_ABOUT_SITE_DESC', 'Site Map Description');
+define('_MI_TADSITEMA_ABOUT_SITE_DEFAULT', '<p>Alt+L : Left Zone<br>Alt+C : Content and Center Zone.<br>Alt+R : Right Zone<br>Alt+U : Nav Bar<br></p>');
 
 define('_MI_TADSITEMA_DIRNAME', basename(dirname(dirname(__DIR__))));
 define('_MI_TADSITEMA_HELP_HEADER', __DIR__ . '/help/helpheader.html');
